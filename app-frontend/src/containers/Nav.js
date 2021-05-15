2f0b9ec898f04784f3ca5eea00dcdb0ec7465042
HuydDo <huydinhdo@yahoo.com>
2021-05-15 17:12:36 -0600

    added port number and modified Nav.js

--- app-frontend/src/containers/Nav.js
+++ app-frontend/src/containers/Nav.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Nav () {
     return(
       <div className="nav">
-        <img className="logo" src="../sample_logo.png" alt="sample logo"/>
+       <h3>To Do List</h3>
+        {/* <img className="logo" src="../sample_logo.png" alt="sample logo"/> */}
       </div>
     )
 }
